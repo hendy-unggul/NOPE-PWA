e27c6b5dfb62a6d03b04bf7fd731fc547dfe53c3
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2025-12-12 20:12:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,24 +5,15 @@ const urlsToCache = [
   '/onboarding.html',
   '/inner.html',
   '/luar.html',
-  '/register.html',
   '/artefak-upload.html',
   '/manifest.json',
   '/favicon.ico',
-  '/NOPE 192.png',
-  '/NOPE 512.png'
+  '/logo-nope-192.png',
+  '/logo-nope-512.png'
 ];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
